42002d46c42ef17cb4fa93ded90744c392302f8d
pinoa1228 <pinoa1228@gmail.com>
2022-01-18 11:58:36 +0900

    1/17-변경

---
+++ 문자열/싸이버개강총회.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+import sys
+input = sys.stdin.readline
+
+dict=defaultdict(list)
+def change(time):
+    h,m=time.split(":")
+    return int(h)*60+int(m)
+times = list(input().split())
+# 처음부터 다 계산하려니 시간초과남
+S = int(times[0][:2]+times[0][3:])
+E = int(times[1][:2]+times[1][3:])
+Q = int(times[2][:2]+times[2][3:])
+cnt=0
+arr=[]
+while True:
+    # print(log)
+    try:
+        time,name=input().split()
+        # print(time,name)
+        time = int(time[:2]+time[3:])
+        # print(change_result)
+        if time<=S:
+            dict[name]=1
+        elif E<=time<=Q and dict[name]==1:
+            cnt+=1
+            #이거 생각 못함
+            dict[name]=0
+    except:
+        break
+        
+print(cnt)
+        
+    
